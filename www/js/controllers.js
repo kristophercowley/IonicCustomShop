08694b12912299bfb52d0ce3e3398b3eb9a698e8
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-02-06 23:12:33 -0700

    Populated dropdown with shirts array

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,10 @@ angular.module('app.controllers', [])
 })
    
 .controller('t-ShirtDesignerCtrl', function($scope) {
-
+        $scope.test = "Is this working?";
+        $scope.shirts = [];
+        $scope.shirts.push({color:"red",photo:"url"},{color:"black",photo:"url2"},{color:"blue",photo:"url3"},{color:"brown",photo:"url4"},{color:"yellow",photo:"url"});
+        console.log($scope.shirts);
 })
    
 .controller('shoppingCartCtrl', function($scope) {
